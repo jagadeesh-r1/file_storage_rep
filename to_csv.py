9d731a285658067c0d8071cbe749bca703e0a3c4
jagadeesh-r1 <mail4jagadeeshreddy@gmail.com>
2020-02-18 13:32:10 +0000

    dine done ding

---
+++ to_csv.py
@@ -0,0 +1,15 @@
+import csv
+import os
+
+csv_file = open('data.csv','w')
+csv_writer = csv.writer(csv_file)
+
+csv_writer.writerow(['path','image_name'])
+
+for a,b,c in os.walk('Amazon_USA'):
+	if len(c)>0:
+		for i in c:
+			csv_writer.writerow([a,i])
+
+csv_file.close()
+
