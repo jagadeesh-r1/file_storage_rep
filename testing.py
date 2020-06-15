46114dd794ea12f7e3e7b9c0eb0687eb2291aa4d
Jagadeesh Reddy Vanga <31398032+jagadeesh-r1@users.noreply.github.com>
2020-06-15 18:24:29 +0530

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,30 @@
+from google.cloud import storage
+from csv import reader
+
+def upload_to_bucket(blob_name, path_to_file, bucket_name):
+    """ Upload data to a bucket"""
+
+    # Explicitly use service account credentials by specifying the private key
+    # file.
+    storage_client = storage.Client.from_service_account_json(
+        'creds.json')
+
+    #print(buckets = list(storage_client.list_buckets())
+
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(path_to_file)
+
+    #returns a public url
+    return blob.public_url
+
+ 
+# open file in read mode
+with open('done_data.csv', 'r') as read_obj:
+    # pass the file object to reader() to get the reader object
+    csv_reader = reader(read_obj)
+    # Iterate over each row in the csv using reader object
+    for row in csv_reader:
+        # row variable is a list that represents a row in csv
+        print(row)
+        # print(upload_to_bucket('image_classification_dataset/ebay',row,'crawling_testing'))
