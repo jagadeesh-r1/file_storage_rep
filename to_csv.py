a359aecf2de2a5fd6486b4f06ef5e83f6f3cf18e
Jagadeesh Reddy Vanga <31398032+jagadeesh-r1@users.noreply.github.com>
2020-06-15 11:42:04 +0530

    changed marketplace

--- to_csv.py
+++ to_csv.py
@@ -6,7 +6,7 @@ csv_writer = csv.writer(csv_file)
 
 csv_writer.writerow(['path','image_name'])
 
-for a,b,c in os.walk('Amazon_USA'):
+for a,b,c in os.walk('eBay'):
 	if len(c)>0:
 		for i in c:
 			csv_writer.writerow([a,i])
